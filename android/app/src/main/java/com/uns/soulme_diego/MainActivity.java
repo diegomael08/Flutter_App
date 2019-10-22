e648aa178b1695c7d1bf96a09e0c2824f06f4d8f
unknown <diegoma_980@hotmail.com>
2019-10-22 09:31:37 -0500

    app soulme

---
+++ android/app/src/main/java/com/uns/soulme_diego/MainActivity.java
@@ -0,0 +1,13 @@
+package com.uns.soulme_diego;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
